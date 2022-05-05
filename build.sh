e34773862c88f9a37814f05f7a5f357ff6b24ca0
Gonzalo Matarrubia <g.matarrubia@asysnet.es>
2022-05-05 23:34:02 +0200

    move conf to DISTRO conf file

--- build.sh
+++ build.sh
@@ -6,56 +6,29 @@ export MACHINE=raspberrypi3
 rm -f "$MACHINE/conf/local.conf"
 
 source ./setup-environment "$MACHINE"
-#echo -e 'CORE_IMAGE_EXTRA_INSTALL += " \' >> conf/local.conf
-#echo -e '  flutter-pi \' >> conf/local.conf
-#echo -e '  flutter-drm-gbm-backend \' >> conf/local.conf
-#echo -e '"\n' >> conf/local.conf
-echo -e 'CONNECTIVITY_CHECK_URIS = "https://www.yoctoproject.org/"' >> conf/local.conf
-
 
 echo '***************************************'
+echo -e 'CONNECTIVITY_CHECK_URIS = "https://www.yoctoproject.org/"' >> conf/local.conf
+echo -e 'DISTRO = "flutterpi"' >> ./conf/local.conf
 echo -e "DL_DIR = \"$repoPath/dl\"" >> ./conf/local.conf
 echo -e "SSTATE_DIR = \"$repoPath/sstate\"" >> ./conf/local.conf
 echo -e "SSTATE_MIRRORS = \"file://.* file://$repoPath/sstate/PATH\"" >> ./conf/local.conf
 echo -e 'SSTATE_MIRRORS += "file://.* http://sstate.yoctoproject.org/honister/PATH;downloadfilename=PATH"' >> ./conf/local.conf
-echo -e 'IMAGE_LINGUAS = "es-Es"' >> ./conf/local.conf
-echo -e 'PACKAGECONFIG:pn-sascha-samples = "d2d"' >> ./conf/local.conf
-echo -e 'PACKAGECONFIG:pn-vkcube = "kms wayland"' >> ./conf/local.conf
-echo -e 'PREFERRED_PROVIDER:jpeg = "libjpeg-turbo"' >> ./conf/local.conf
-echo -e 'PREFERRED_PROVIDER:jpeg-native = "libjpeg-turbo-native"' >> ./conf/local.conf
-echo -e '' >> ./conf/local.conf
-echo -e 'PREFERRED_VERSION:vulkan-headers = "1.2.198.0"' >> ./conf/local.conf
-echo -e 'PREFERRED_VERSION:vulkan-loader = "1.2.198.1"' >> ./conf/local.conf
-echo -e 'PREFERRED_VERSION:vulkan-tools = "1.2.198.0"' >> ./conf/local.conf
-echo -e 'PREFERRED_VERSION:glslang = "11.6.0"' >> ./conf/local.conf
-echo -e 'PREFERRED_VERSION:vulkan-validationlayers = "1.2.198"' >> ./conf/local.conf
-echo -e 'PREFERRED_VERSION:cmake = "3.21.1"' >> ./conf/local.conf
-echo -e 'PREFERRED_VERSION:cmake-native = "3.21.1"' >> ./conf/local.conf
-echo '********** ./conf/local.conf **********'
-cat ./conf/local.conf
-echo '***************************************'
-bitbake-layers show-layers
-echo '***************************************'
-bitbake -e virtual/kernel | grep "^PV"
-bitbake -e virtual/kernel | grep "^PN"
-echo '***************************************'
-bitbake -e core-image-minimal | grep "^DISTRO_FEATURES"
-echo '***************************************'
+
+bitbake-layers add-layer ../meta-config-pi
+bitbake-layers add-layer ../meta-flutter-app
+
+##NOTE Enable verbose option
+#echo '********** ./conf/local.conf **********'
+#cat ./conf/local.conf
+#echo '***************************************'
+#bitbake-layers show-layers
+#echo '***************************************'
+#bitbake -e virtual/kernel | grep "^PV"
+#bitbake -e virtual/kernel | grep "^PN"
+#echo '***************************************'
+#bitbake -e core-image-minimal | grep "^DISTRO_FEATURES"
+#echo '***************************************'
 bitbake -e > bb.environment
-
-echo 'CORE_IMAGE_EXTRA_INSTALL:append = " \
-              flutter-pi-release \
-              flutter-gallery-release \
-              swiftshader \
-              vkcube \
-              vkquake3 \
-              vkrunner \
-              vk-layer-perfdoc \
-              gstreamer1.0-libav \
-              gstreamer1.0-plugins-bad \
-              gstreamer1.0-plugins-good \
-              gstreamer1.0-plugins-ugly \
-              "' >> ./conf/local.conf
-
-#. ./sources/poky/oe-init-build-env "$MACHINE"
+#bitbake rpi-weather -D
 time bitbake core-image-minimal
