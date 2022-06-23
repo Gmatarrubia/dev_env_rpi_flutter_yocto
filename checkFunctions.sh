6f5938d248952ac62229b7f2e8445743c29f9338
Gonzalo Matarrubia <g.matarrubia@asysnet.es>
2022-06-23 19:38:46 +0200

    complete check_docker function

--- checkFunctions.sh
+++ checkFunctions.sh
@@ -48,14 +48,29 @@ function check_kas(){
 }
 
 function check_docker(){
+    local user=$(id -nu)
     if [ -n "$(which docker)" ]
     then
         echo "Docker already installed"
     else
+        echo "Installing docker..."
         sudo apt install -y curl
         curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
-        sudo sh /tmp/get-docker.sh
+        sudo sh /tmp/get-docker.sh > /dev/null
     fi
+    # Check if current user is in the docker group
+    is_user_in_docker_group=$(id | grep -c docker)
+    if [ "$is_user_in_docker_group" -eq 1 ]
+    then
+        echo "User already added to docker group"
+    else
+        echo "Adding user $user to docker group."
+        echo "Sudo password needed."
+        sudo usermod -aG docker "$user"
+        echo "Reboot your system. Then, try it again!"
+        return 1
+    fi
+    return 0
 }
 
 # Print help
